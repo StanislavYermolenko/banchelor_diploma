86752207955eb768a08a7fc64523a9fdfdb855f1
adamant3773 <87760289+adamant3773@users.noreply.github.com>
2021-07-22 16:59:23 +0300

    Add files via upload
    one

---
+++ mod/bucket/variables.tf
@@ -0,0 +1,7 @@
+variable "image_bucket_name" {}
+
+variable "bucket_location" {}
+
+variable "destroy_policy" {
+  type = bool
+}
---
+++ mod/in_group/variables.tf
@@ -0,0 +1,15 @@
+variable "instance_network" {}
+
+variable "startup_script" {}
+
+variable "service_account_for_instance" {}
+
+variable "template_name" {}
+
+variable "instance_machine_type" {}
+
+variable "igm_region" {}
+
+variable "instance_tag" {}
+
+variable "boot_disc_image" {}
